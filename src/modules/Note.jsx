0c1dd2a1b1108a017f48142a4702e416f721b89f
Jon-PR0 <jonathan.almeida1793@gmail.com>
2022-10-16 16:04:03 -0300

    Starting

--- src/modules/Note.jsx
+++ src/modules/Note.jsx
@@ -7,7 +7,7 @@ export default function Note({ isActive, setIsActive, setShowNotes }) {
         <RetrunButton section={setIsActive} previousSection={setShowNotes} top="16" left="16" />
         <h2 className="font-bold text-3xl sm:text-4xl max-w-md text-center m-auto text-primaryColor-500 mt-4 mb-6">Gerrit Losh: Não guarde ressentimento</h2>
         <p className="font-medium text-sm sm:text-md text-center text-primaryColor-400 mt-4 mb-6">1 Coríntios 12:16</p>
-        <p className="font-normal text-base sm:text-lg text-center text-primaryColor-200 mt-4 mb-6">Startup Framework contains components and complex blocks which can easily be integrated into almost any design. All of the components are made in the same style, and can easily be integrated into projects, allowing you to create hundreds of solutions.</p>
+        <p className="font-normal text-base sm:text-lg text-center text-primaryColor-200 mt-4 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, atque praesentium. Odio temporibus aliquid aspernatur pariatur dicta blanditiis sed culpa magni, natus rerum sapiente ab quibusdam deserunt ipsa ad! Fuga.</p>
         <ul className="flex gap-4 justify-around flex-wrap font-medium text-sm sm:text-lg text-primaryColor-400 ">
           <li>2 Timóteo 2:44</li>
           <li>Apocalipse 21:3,4</li>
